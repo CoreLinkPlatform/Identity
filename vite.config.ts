cb96438edfe5b1b79a20028bfe919fb10bfa9934
Javid <javid.momeni@gmail.com>
2026-08-30 10:20:52 +0200

    feat: make theme name configurable

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
 
+const themeName = process.env.KEYCLOAK_THEME_NAME ?? "corelink";
+
 export default defineConfig({
   plugins: [
     react(),
     keycloakify({
-      themeName: "corelink",
+      themeName,
       accountThemeImplementation: "none",
       keycloakVersionTargets: {
-        "26.2-and-above": "corelink-26.2-and-above.jar"
+        "26.2-and-above": `${themeName}-26.2-and-above.jar`
       }
     })
   ]
