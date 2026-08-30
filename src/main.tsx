1f5d00d4161915b3b46a56bf930f0da890859427
Javid <javid.momeni@gmail.com>
2026-08-30 17:55:08 +0200

    fix(login): load self-contained control icons

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { KcPage } from "./kc.gen";
 import "./styles/corelink.css";
+import "./styles/login-controls.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
